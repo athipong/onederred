456d6df081044275f66f1279dab87f85d7ea56e7
testweb <bbcro_001@hotmail.com>
2025-07-26 00:43:27 +0700

    เพิ่มไฟล์ index.html

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyCKuK-XsRn1kpreS210wqP_uoiwUANQ5r8",
+    authDomain: "tb-git.firebaseapp.com",
+    projectId: "tb-git",
+    storageBucket: "tb-git.firebasestorage.app",
+    messagingSenderId: "311209997293",
+    appId: "1:311209997293:web:90aaaea62c2a4f80d6ede4",
+    measurementId: "G-FSSQZM86HR"
+  };
